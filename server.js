43717455c21b2f4311f71b8441e56423cdc43256
Jiho Kim <jihok0519@gmail.com>
2022-05-16 20:57:49 -0400

    Added if statement if bottles = 0

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:number_of_bottles', (req, res) => {
-    res.send(req.params.number_of_bottles + ` Bottles of beer on the wall <a href="localhost:3000/">Take one down, pass it around</a>`);
+    res.send(req.params.number_of_bottles + ` Bottles of beer on the wall <a href="localhost:3000/${(req.params.number_of_bottles - 1)}">Take one down, pass it around</a>`);
+    if (req.params.number_of_bottles === 0) {
+        res.send(`<a href="localhost:3000/">Start Over!</a>`)
+    }
 });
 
 app.listen(port, () => {
